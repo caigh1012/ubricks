9db0b6ac20af635fb0479fddb9811c84a708c2c2
caigh1012 <19925338175@163.com>
2024-07-10 20:02:22 +0800

    feat: add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@lib', './lib']],
+        extensions: ['.ts', '.js'],
+      },
+    },
+  },
+  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  plugins: ['import', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 2,
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+    {
+      files: ['*.ts'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {},
+    },
+  ],
+};
